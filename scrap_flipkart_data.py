c1aa573fad1fd5c4834439ac9fccd6619e41b2d1
Asmita Giri <asmitagiri145@gmail.com>
2025-12-31 14:35:01 +0545

    Got the products name of 1st page

--- scrap_flipkart_data.py
+++ scrap_flipkart_data.py
@@ -1,6 +1,28 @@
 import pandas as pd 
 from bs4 import BeautifulSoup 
 import requests
-url="https://www.flipkart.com"
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+product_name=[]
+product_price=[]
+product_review=[]
+product_description=[]
+# for i in range(2,10):
+url="https://www.flipkart.com/search?q=mobile+under+50000&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page=1"
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
 r=requests.get(url)
-print(r)
+# print(r)
+soup=BeautifulSoup(r.text,"html.parser")
+names=soup.find_all("div",class_="RG5Slk")
+for i in names:
+    product_name.append(i.text)
+    print(i.text)
+
+print("Total mobiles found:", len(product_name))
+# print(soup.prettify())
+# np=soup.find("a",class_="jgg0SZ").get("href")
+# cnp="https://www.flipkart.com"+np
+# print(cnp)
